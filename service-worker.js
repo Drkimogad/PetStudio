7d675bdb964a0356ffa2fcc24a5b41e52674d433
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-02-06 16:15:27 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -48,18 +48,18 @@ self.addEventListener('fetch', (event) => {
 
             // If the request is for an HTML file (navigation), return the offline page
             if (event.request.mode === 'navigate') {
-                return caches.match('https://drkimogad.github.io/PetStudio/offline.html');  // Ensure offline.html is cached
+                return caches.match('https://drkimogad.github.io/PetStudio/index.html');  // Ensure offline.html is cached
             }
 
             console.log('Fetching from network:', event.request.url);
             return fetch(event.request).catch(() => {
                 // Offline fallback if fetch fails (e.g., user is offline)
-                return caches.match('https://drkimogad.github.io/PetStudio/offline.html');  // Ensure offline.html is cached
+                return caches.match('https://drkimogad.github.io/PetStudio/index.html');  // Ensure offline.html is cached
             });
         }).catch((err) => {
             console.error('Error fetching:', err);
             // In case of any unexpected errors, fallback to offline.html
-            return caches.match('https://drkimogad.github.io/PetStudio/offline.html');
+            return caches.match('https://drkimogad.github.io/PetStudio/index.html');
         })
     );
 });
