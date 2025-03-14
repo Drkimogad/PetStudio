3a6ba810eb3593c71a37bb09eecdaee4abfa09b2
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-14 18:12:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -174,7 +174,7 @@ function subscribeUserToPushNotifications(registration) {
 
 // Function to send subscription to server
 function sendSubscriptionToServer(subscription) {
-    fetch('https://drkimogad.github.io/PetStudio/api/send-reminders.js', {
+    fetch('https://drkimogad.github.io/PetStudio/firebase-messaging-sw.js', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ subscription }),
