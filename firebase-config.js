536e7f6500e691ac6917e4cd5535c9d5cd32a5ed
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-14 15:33:37 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB42agDYdC2-LF81f0YurmwiDmXptTpMVw",
   authDomain: "swiftreach2025.firebaseapp.com",
@@ -14,8 +10,11 @@ const firebaseConfig = {
   messagingSenderId: "540185558422",
   appId: "1:540185558422:web:d560ac90eb1dff3e5071b7",
   measurementId: "G-SNBPRVBPNM"
+  vapidKey: "BAL7SL85Z3cAH-T6oDGvfxV0oJhElCpnc7F_TaF2RQogy0gnUChGa_YtmwKdifC4c4pZ0NhUd4T6BFHGRxT79Gk" // Your VAPID key
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const messaging = getMessaging(app); // Get messaging instance
+export { messaging }; // Export messaging
