e018ea606842b29bbf81a86f6de67176ebd13f44
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-17 18:52:27 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getMessaging } from 'firebase/messaging';
+import { getMessaging, getToken } from 'firebase/messaging';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,11 +10,26 @@ const firebaseConfig = {
   messagingSenderId: "540185558422",
   appId: "1:540185558422:web:d560ac90eb1dff3e5071b7",
   measurementId: "G-SNBPRVBPNM"
-  vapidKey: "BAL7SL85Z3cAH-T6oDGvfxV0oJhElCpnc7F_TaF2RQogy0gnUChGa_YtmwKdifC4c4pZ0NhUd4T6BFHGRxT79Gk" // Your VAPID key
-
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app); // Get messaging instance
+
+// Set VAPID key
+getToken(messaging, { vapidKey: "BAL7SL85Z3cAH-T6oDGvfxV0oJhElCpnc7F_TaF2RQogy0gnUChGa_YtmwKdifC4c4pZ0NhUd4T6BFHGRxT79Gk" }).then((currentToken) => {
+  if (currentToken) {
+    console.log('Got FCM device token:', currentToken);
+    // Send the token to your server and update the UI if necessary
+    // ...
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.error('An error occurred while retrieving token. ', err);
+  // ...
+});
+
 export { messaging }; // Export messaging
