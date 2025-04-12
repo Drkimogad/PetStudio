e209b83890fb976850f1ea0286fe2c2ad69f2d4a
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-12 09:54:04 +0200

    Update save-subscription.js

--- api/save-subscription.js
+++ api/save-subscription.js
@@ -1,3 +1,4 @@
+import { db } from './lib/firebase-admin'; // Your existing setup
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS"); // Allow specific HTTP methods
