b149231b29d20734544634828d3837903d24fd93
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-13 16:02:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -752,7 +752,7 @@ qrWindow.document.write(`
             const canvas = document.querySelector('#qrcode-container canvas');
             if (canvas) {
               const link = document.createElement('a');
-              link.download = `${profile.name}_QR.png`;
+              link.download = '${profile.name}_QR.png';
               link.href = canvas.toDataURL();
               link.click();
             }
