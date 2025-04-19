bce4f82a3d4b83e63f721d8783a430d35efb9044
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-19 18:53:47 +0200

    Update auth-init.js

--- auth-init.js
+++ auth-init.js
@@ -1,14 +1,20 @@
 // auth-init.js
 function loadGoogleAPIs() {
+  // Load gapi.js
   const gapiScript = document.createElement('script');
   gapiScript.src = 'https://apis.google.com/js/api.js';
-  gapiScript.onload = () => window.gapiLoaded = true;
+  gapiScript.onload = () => {
+    window.gapiLoaded = true;
+    console.log('Google API client loaded');
+  };
   document.head.appendChild(gapiScript);
 
+  // Load GSI client
   const gisScript = document.createElement('script');
   gisScript.src = 'https://accounts.google.com/gsi/client';
-  gisScript.onload = () => window.gisLoaded = true;
+  gisScript.onload = () => {
+    window.gisLoaded = true;
+    console.log('Google Identity Services loaded');
+  };
   document.head.appendChild(gisScript);
 }
-
-// Initialize after Firebase
