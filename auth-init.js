69c06d5ffeb356d0a60664351a5f78a6f0da4ca5
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-19 18:23:00 +0200

    Update auth-init.js

--- auth-init.js
+++ auth-init.js
@@ -1,3 +1,10 @@
 // auth-init.js
-function onGapiLoad() { window.gapiLoaded = true; }
-function onGisLoad() { window.gisLoaded = true; }
+function onGapiLoad() { 
+  window.gapiLoaded = true;
+  console.log("Google API client loaded");
+}
+
+function onGisLoad() { 
+  window.gisLoaded = true;
+  console.log("Google Identity Services loaded");
+}
