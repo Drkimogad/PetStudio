a22b51e21d880bda32e49e8f3331799ef696dd39
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-25 19:10:39 +0200

    Update auth-init.js

--- auth-init.js
+++ auth-init.js
@@ -1,20 +1,15 @@
 // auth-init.js
 function loadGoogleAPIs() {
-  // Load gapi.js
+  const nonce = 'YourRandomNonceValue'; // Must match CSP nonce
+  
   const gapiScript = document.createElement('script');
+  gapiScript.nonce = nonce;
   gapiScript.src = 'https://apis.google.com/js/api.js';
-  gapiScript.onload = () => {
-    window.gapiLoaded = true;
-    console.log('Google API client loaded');
-  };
-  document.head.appendChild(gapiScript);
-
-  // Load GSI client
-  const gisScript = document.createElement('script');
-  gisScript.src = 'https://accounts.google.com/gsi/client';
-  gisScript.onload = () => {
-    window.gisLoaded = true;
-    console.log('Google Identity Services loaded');
-  };
-  document.head.appendChild(gisScript);
+  gapiScript.onload = () => console.log('Google API loaded');
+  
+  const gsiScript = document.createElement('script');
+  gsiScript.nonce = nonce;
+  gsiScript.src = 'https://accounts.google.com/gsi/client';
+  
+  document.head.append(gapiScript, gsiScript);
 }
