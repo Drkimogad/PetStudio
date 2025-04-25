1f947c46b05dfd9669efea2cc6386e2ca3d47d88
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-25 23:13:14 +0200

    Update auth-init.js

--- auth-init.js
+++ auth-init.js
@@ -1,15 +1,15 @@
 function loadGoogleAPIs() {
   // Load gapi.js with integrity check
-  const gapiScript = document.createElement('script');
-  gapiScript.src = 'https://apis.google.com/js/api.js';
-  gapiScript.integrity = 'sha256-...'; // Get from https://cdn.jsdelivr.net/npm/apis.google.com/js/api.js
-  gapiScript.crossOrigin = 'anonymous';
+const gsiScript = document.createElement('script');
+gsiScript.src = 'https://accounts.google.com/gsi/client';
+gsiScript.integrity = 'sha256-+0JEH4h4yMMMF99dG0OZ8j4XlNk9XigLxE7k9wuRj5E=';
+gsiScript.crossOrigin = 'anonymous';
   
   // Load GSI client with integrity check
-  const gsiScript = document.createElement('script');
-  gsiScript.src = 'https://accounts.google.com/gsi/client';
-  gsiScript.integrity = 'sha256-...'; // Get from https://cdn.jsdelivr.net/npm/accounts.google.com/gsi/client
-  gsiScript.crossOrigin = 'anonymous';
+const gsiScript = document.createElement('script');
+gsiScript.src = 'https://accounts.google.com/gsi/client';
+gsiScript.integrity = 'sha256-+0JEH4h4yMMMF99dG0OZ8j4XlNk9XigLxE7k9wuRj5E=';
+gsiScript.crossOrigin = 'anonymous';
 
   document.head.append(gapiScript, gsiScript);
 }
